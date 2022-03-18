3185f580f288151e88343c4b664e2ed59545fe75
Albongle <alejandro.bongioanni@gmail.com>
2022-03-18 11:52:27 -0300

    cambios login

--- routes/RouteLogin.js
+++ routes/RouteLogin.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
 router.post("/", async (req, res) => {
 
   let respuesta = await usuario.logInUsuario(req.body.user,req.body.password);
-  res.status(200).json(respuesta);
+  respuesta.length == 0 ? res.status(401).json({error:"access denied"}) :res.status(200).json({status:"ok", object:respuesta[0]["_fieldsProto"]});
 });
 
 module.exports = router;
