bed7ac46afd747b1177478054434aeeb3a20d201
harryunla <harryunla@gmail.com>
2022-03-18 11:57:12 -0300

    limpiar metodos no usados

--- routes/RouteUsuario.js
+++ routes/RouteUsuario.js
@@ -11,18 +11,6 @@ router.get("/:id", async (req, res) => {
   let respuesta = await usuario.getUsuario(req.params.id);
   respuesta.exists ? res.status(200).json(respuesta) : res.status(404).json({error:"user not found"});
 
-  // if (req.session.usuario) {
-  //   if (req.query.id) {
-  //     res.json(p.traerusuario(req.query.id));
-  //   } else {
-  //     res.status(400).send("error");
-  //   }
-  // } else {
-  //   //aca redirigir al login usuario no logeado
-  //   res.status(400).redirect("/api/login");
-  // }
-});
-
 
 
 module.exports = router;
